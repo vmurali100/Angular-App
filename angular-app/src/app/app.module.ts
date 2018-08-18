d4cae7febb427f59adc11b50b6aa75375a95f7fe
Murali Veesambattu <MVeesambattu@simeiosolutions.com>
2018-08-18 09:54:36 +0530

    Training Component Changes

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { WelcomeComponent } from './welcome/welcome.component'
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './navigation/header/header.component';
 import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
+import { StopTrainingComponent } from './training/current-training/stop-training.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -28,6 +29,7 @@ import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.com
     WelcomeComponent,
     HeaderComponent,
     SidenavListComponent,
+    StopTrainingComponent
   ],
   imports: [
     BrowserModule,
@@ -39,6 +41,7 @@ import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.com
 
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents:[StopTrainingComponent]
 })
 export class AppModule { }
--- angular-app/src/app/training/current-training/current-training.component.ts
+++ angular-app/src/app/training/current-training/current-training.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit , EventEmitter,Output} from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { StopTrainingComponent } from './stop-training.component'
 @Component({
   selector: 'app-current-training',
   templateUrl: './current-training.component.html',
   styleUrls: ['./current-training.component.css']
 })
 export class CurrentTrainingComponent implements OnInit {
-
-  constructor() { }
+  progress=0;
+  timer;
+  @Output() trainingExit = new EventEmitter<void>()
+  constructor( private dailog:MatDialog) { }
 
   ngOnInit() {
+    this.startOrResumeTimer()
   }
+  startOrResumeTimer(){
+    this.timer = setInterval(() => {
+      this.progress = this.progress+10;
 
+      if(this.progress>=100){
+        clearInterval(this.timer)
+      }
+    }, 500);
+  }
+  onStop(){
+    clearInterval(this.timer);
+    const dialogRef = this.dailog.open(StopTrainingComponent,{
+      data:{
+        progress:this.progress
+      }
+    })
+    dialogRef.afterClosed().subscribe(res=>{
+      if(res){
+        this.trainingExit.emit()
+      }else{
+        this.startOrResumeTimer()
+      }
+    })
+  }
 }
